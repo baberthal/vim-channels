5c03d84e0651a6a18b8f151c001fb0bae70784d2
J. Morgan Lieberthal <j.morgan.lieberthal@gmail.com>
2020-11-10 21:05:00 -0700

    Simplify Command class

--- lib/vim_channels/vim/command.rb
+++ lib/vim_channels/vim/command.rb
@@ -26,51 +26,8 @@ module VimChannels
     # Errors in these commands are normally not reported to avoid them messing
     # up the display.  If you do want to see them, set the 'verbose' option (in
     # Vim) to 3 or higher.
-    #
-    # If you decide to subclass this command and implement another vim command,
-    # make sure to override thie following methods:
-    #
-    # * {#initialize} - to initialize an instance of the command subclass.
-    # * {#as_json}    - to return a representation of the instance suitable
-    #   for json serialization. This is typically an Array or a Hash.
-    #
-    # Additionally, you should add a class method to {Command} to create
-    # instances of your class. They should never be instantiated directly.
     class Command
-      # Subclasses must implement {#as_json} to return a representation of the
-      # instance suitable for JSON serialization.
-      #
-      # @return [Array, Hash]
-      def as_json(*)
-        raise NotImplementedError
-      end
-
-      # Returns a JSON representation of the Command.
-      #
-      # @return [String]
-      def to_json(*args)
-        as_json(*args).to_json
-      end
-
-      # Returns a string representation of the Command. Subclasses can override
-      # this method to return a better string representation. The default
-      # implementation simply returns {#to_json}.
-      #
-      # @return [String]
-      def to_s
-        to_json
-      end
-
-      # Returns a string representation of the Command. Subclasses can override
-      # this method to return a better representation. The default
-      # implementation simply returns {#to_s}.
-      #
-      # @return [String]
-      def inspect
-        to_s
-      end
-
-      # Create a {Redraw} command.
+      # Create a redraw command.
       #
       # The other commands do not update the screen, so that you can send
       # a sequence of commands without the cursor moving around.  You must end
@@ -79,12 +36,12 @@ module VimChannels
       #
       # @param force [Boolean] Pass `true` to force the redraw in vim.
       #
-      # @return [Command::Redraw] The redraw command.
+      # @return [(String, String)] The redraw command.
       def self.redraw(force: false)
-        Redraw.new(force)
+        ["redraw", force ? "force" : ""]
       end
 
-      # Create an {Ex} command.
+      # Create an ex command.
       #
       # The "ex" command is executed as any Ex command.  There is no response
       # for completion or error. You could use functions in an autoload script:
@@ -99,9 +56,9 @@ module VimChannels
       #
       # @param command [String] The ex command to execute.
       #
-      # @return [Command::Ex] The ex command.
+      # @return [(String, String)] The ex command.
       def self.ex(command)
-        Ex.new(command)
+        ["ex", command]
       end
 
       # Create a normal-mode command.
@@ -114,9 +71,9 @@ module VimChannels
       #
       # @param command [String] The normal mode command to execute.
       #
-      # @return [Command::Normal] The normal mode command.
+      # @return [(String, String)] The normal mode command.
       def self.normal(command)
-        Normal.new(command)
+        ["normal", command]
       end
 
       # Create an expression command.
@@ -156,11 +113,14 @@ module VimChannels
       # @param expression [String] The expression to evaluate.
       # @param id [Integer] An optional id for the expression. If this is set,
       #                     vim will send a response to the server with the
-      #                     result of the expression.
+      #                     result of the expression. **Note that the id must be
+      #                     negative**
       #
-      # @return [Command::Expr] The expression.
-      def self.expr(expression, id: nil)
-        Expr.new(expression, id: id)
+      # @return [(String, String), (String, String, Integer)] The expression.
+      def self.expr(expression, id = nil)
+        ary = ["expr", expression]
+        ary << id if id
+        ary
       end
 
       # Create a function call command.
@@ -183,85 +143,11 @@ module VimChannels
       # @param id [Integer] An optional id. If this is set, vim will send
       #                     a response to the server.
       #
-      # @return [Command::Call] A `Call` command.
-      def self.call(function, args, id: nil)
-        Call.new(function, args, id: id)
-      end
-
-      # Representation of a `redraw` command.
-      class Redraw < Command
-        # @api private
-        def initialize(force)
-          @force = force
-        end
-
-        # (see Command#as_json)
-        def as_json(*)
-          ["redraw", @force ? "force" : ""]
-        end
-      end
-
-      # Representation of an `ex` command.
-      class Ex < Command
-        # @api private
-        def initialize(command)
-          @command = command.to_s
-        end
-
-        # (see Command#as_json)
-        def as_json(*)
-          ["ex", @command]
-        end
-      end
-
-      # Representation of a `normal` mode command.
-      class Normal < Command
-        # @api private
-        def initialize(command)
-          @command = command.to_s
-        end
-
-        # (see Command#as_json)
-        def as_json(*)
-          ["normal", @command]
-        end
-      end
-
-      # Representation of an `expr` command.
-      class Expr < Command
-        # @api private
-        def initialize(expression, id: nil)
-          @expression = expression.to_s
-          @id = id
-        end
-
-        # (see Command#as_json)
-        def as_json(*)
-          if id = @id
-            ["expr", @expression, id]
-          else
-            ["expr", @expression]
-          end
-        end
-      end
-
-      # Representation of a `call` command.
-      class Call < Command
-        # @api private
-        def initialize(function, args, id: nil)
-          @function = function.to_s
-          @args = args
-          @id = id
-        end
-
-        # (see Command#as_json)
-        def as_json(*)
-          if id = @id
-            ["call", @function, @args, id]
-          else
-            ["call", @function, @args]
-          end
-        end
+      # @return [(String, Array, Integer), (String, Array)] A call command.
+      def self.call(function, args, id = nil)
+        ary = ["call", function, args]
+        ary << id if id
+        ary
       end
     end
   end
--- spec/vim_channels/vim/command_spec.rb
+++ spec/vim_channels/vim/command_spec.rb
@@ -2,34 +2,57 @@
 
 RSpec.describe VimChannels::Vim::Command do
   describe ".redraw" do
-    it "returns a Redraw command" do
-      expect(described_class.redraw).to be_a VimChannels::Vim::Command::Redraw
+    it "with no arguments, returns ['redraw', '']" do
+      expect(described_class.redraw).to eq ["redraw", ""]
+    end
+
+    it "returns ['redraw', 'force'] if force: true" do
+      expect(described_class.redraw(force: true)).to eq %w[redraw force]
     end
   end
 
   describe ".ex" do
-    it "returns an Ex command" do
-      expect(described_class.ex("cmd")).to be_a VimChannels::Vim::Command::Ex
+    it "returns an array with 'ex' as the 1st element, and the command 2nd" do
+      cmd = "if mode() == 'i' | call feedkeys('ClassName') | endif"
+      expect(described_class.ex(cmd)).to eq ["ex", cmd]
     end
   end
 
   describe ".normal" do
-    it "returns a Normal command" do
-      expect(described_class.normal("gg")).to \
-        be_a VimChannels::Vim::Command::Normal
+    it "returns an array with 'normal' as the 1st element, and the cmd 2nd" do
+      expect(described_class.normal("zO")).to eq %w[normal zO]
     end
   end
 
   describe ".expr" do
-    it "returns an Expr command" do
-      expect(described_class.expr("&sw")).to be_a VimChannels::Vim::Command::Expr
+    context "when a result is expected and an ID is passed" do
+      it "returns an array with 'expr', the expression, and the id" do
+        expect(described_class.expr("line('$')", -2)).to \
+          eq ["expr", "line('$')", -2]
+      end
+    end
+
+    context "when no response is expected and no id is passed" do
+      it "returns an array with 'expr' and the expression" do
+        expect(described_class.expr("setline('$'), ['one', 'two', 'three']"))
+          .to eq ["expr", "setline('$'), ['one', 'two', 'three']"]
+      end
     end
   end
 
   describe ".call" do
-    it "returns a Call command" do
-      expect(described_class.call("fn", ["args"])).to \
-        be_a VimChannels::Vim::Command::Call
+    context "when a response is expected and an id is passed" do
+      it "returns an array with 'call', the func name, args, and an id" do
+        expect(described_class.call("line", ["$"], -2))
+          .to eq ["call", "line", ["$"], -2]
+      end
+    end
+
+    context "when response is expected and no id is passed" do
+      it "returns an array with 'call', the func name, and arguments" do
+        expect(described_class.call("setline", ["$", %w[one two three]]))
+          .to eq ["call", "setline", ["$", %w[one two three]]]
+      end
     end
   end
 end
